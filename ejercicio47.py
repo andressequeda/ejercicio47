c93c220a06ab4583c48d6fc2d5f66048d990c3ae
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 10:10:28 -0500

    Add files via upload

---
+++ ejercicio47.py
@@ -0,0 +1,6 @@
+numero  =  int ( input ( "Ingrese el primer número:" ))
+numero_mayor  =  int ( input ( "Ingrese un número mayor al primer número:" ))
+if  numero_mayor  >  numero :
+    print ( suma ( rango ( numero , numero_mayor  +  1 )))
+otra cosa :
+    print ( "Error. El segundo número es menor o igual que el primero" )
